bd91257f24d210b995c0f61df51d8e076dfa10cc
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2017-07-08 17:23:33 +0100

    First multiple select set up

--- js/super_select.js
+++ js/super_select.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
     $('#myselector').on('change', function() {
         var selected = $(this).val();
-        if (selected != "") {
+        var multiple = $(this).attr('data-ismultiple');
+        if ( multiple && (selected != "") ) {
+            selectedHTML = generateHTML(selected);
 
+            $('.selected-elements').append(selectedHTML);
         }
     });
-
+    function generateID() {
+      // Math.random should be unique because of its seeding algorithm.
+      // Convert it to base 36 (numbers + letters), and grab the first 9 characters after the decimal.
+      return '_' + Math.random().toString(36).substr(2, 9);
+    };
     function generateHTML(value) {
-        var result_html = '<div class="single-element">';
-        result_html += '</div>';
+        // generate a unique id for this element 
+        var elem_id = generateID();
+        var result_html = '<span id="'+ elem_id  +'" data-value="'+ value +'" class="signle-element">'+ value +'</span>';
+        return result_html;
     }
 });
