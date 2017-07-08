2be70c1cd950de77d34a82975dc1edb975b8c90d
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2017-07-08 16:49:11 +0100

    Added starter files

---
+++ js/super_select.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#myselector').on('change', function() {
+        var selected = $(this).val();
+        if (selected != "") {
+
+        }
+    });
+
+    function generateHTML(value) {
+        var result_html = '<div class="single-element">';
+        result_html += '</div>';
+    }
+});
